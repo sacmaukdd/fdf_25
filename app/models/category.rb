107cfa5161bc473f38321e3ff17689cce9116073
Le Van Tho <lvtho@gmail.com>
2017-01-11 14:27:12 +0700

    admin-category

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,7 @@
 class Category < ApplicationRecord
   has_many :products, dependent: :destroy
+
+  validates :name, uniqueness: true, presence: true
+
+  scope :order_date_desc, -> {order created_at: :desc}
 end
